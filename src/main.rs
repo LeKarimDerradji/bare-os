9dc35de7d0140d43c58d81569e96e539936322fe
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-20 11:07:42 +0100

    comment

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
-#![no_std]
-#![no_main]
+#![no_std] // Disable Rust standard library
+#![no_main] // Disable all Rust-level entry points.
 
-use core::panic::PanicInfo;
+use core::panic::PanicInfo; // Import PanicInfo from panic in the core lib
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop{}
-}
-
-#[no_mangle]
+#[no_mangle] // Disable name mangling on this function
+// This function will be called on start as an entry point
 pub extern "C" fn _start() -> ! {
-    loop{}
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
 }
 
+// Create a panic_handler 
+#[panic_handler]
+// This function is called on panic 
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
