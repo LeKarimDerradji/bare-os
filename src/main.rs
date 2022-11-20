56aeb5f56d5448cba23e1bc096b38ee599dfb7ef
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-20 10:57:32 +0100

    start_lang_item

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 #![no_std]
+#![no_main]
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{};
+    loop{}
 }
-fn main() {}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop{}
+}
+
