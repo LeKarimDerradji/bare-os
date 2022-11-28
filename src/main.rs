0d95254fd1b321562bb4b455a4ed248cb7e7a92a
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-28 19:36:50 +0100

    bootstrap_test

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 #![no_std] // Disable Rust standard library
 #![no_main] // Disable all Rust-level entry points.
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
 mod vga_buffer;
 use core::panic::PanicInfo; // Import PanicInfo from panic in the core lib
+
+
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
 
 // Disable name mangling on this function
 // This function will be called on start as an entry point
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
     loop {}
 }
 
