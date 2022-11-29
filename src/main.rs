290c4f4b0101271f9b3fe75cf5f80a904d2b28be
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-29 22:08:56 +0100

    Testable_Trait

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,30 @@
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-mod vga_buffer;
 mod serial;
+mod vga_buffer;
 use core::panic::PanicInfo; // Import PanicInfo from panic in the core lib
 
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+}
+
 #[cfg(test)]
-fn test_runner(tests: &[&dyn Fn()]) {
+fn test_runner(tests: &[&dyn Testable]) {
     serial_println!("Running {} tests", tests.len());
     for test in tests {
-        test();
+        test.run();
     }
     exit_qemu(QemuExitCode::Success);
 }
@@ -27,9 +42,7 @@ fn panic(info: &PanicInfo) -> ! {
 
 #[test_case]
 fn trivial_assertion() {
-    serial_print!("trivial assertion...");
-    assert_eq!(0, 1);
-    println!("[ok]");
+    assert_eq!(1, 1);
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -61,7 +74,7 @@ pub extern "C" fn _start() -> ! {
 }
 
 // Create a panic_handler
-#[cfg(not(test))] 
+#[cfg(not(test))]
 #[panic_handler]
 // This function is called on panic
 fn panic(_info: &PanicInfo) -> ! {
