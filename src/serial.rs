f1a3290249574314fefeb34cefaf1f59f6ac36f8
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-29 21:44:38 +0100

    _print_to_serial

--- src/serial.rs
+++ src/serial.rs
@@ -1,5 +1,3 @@
-
-
 use uart_16550::SerialPort;
 use spin::Mutex;
 use lazy_static::lazy_static;
@@ -10,4 +8,10 @@ lazy_static! {
         serial_port.init();
         Mutex::new(serial_port)
     };
+}
+
+#[doc(hidden)]
+pub fn _print(args: ::core::fmt::Arguments) {
+    use core::fmt::Write;
+    SERIAL1.lock().write_fmt(args).expect("Printing to serial failed");
 }
