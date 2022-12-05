3b980e92ea3a1c820cb8710a567091d83c586712
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-05 23:42:31 +0100

    trait_impl

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,18 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use core::panic::PanicInfo;
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+    {
+        fn run(&self) {
+            serial_print!("{}...\t", core::any::type_name::<T>());
+            self();
+            serial_println!("[ok]");
+    }
