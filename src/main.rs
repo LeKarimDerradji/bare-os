55e2d8ac31234294cdd6ced38f827c5b6af1ca18
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-07 21:36:08 +0100

    moving_exit_qemu

--- src/main.rs
+++ src/main.rs
@@ -45,21 +45,7 @@ fn trivial_assertion() {
     assert_eq!(1, 1);
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Success = 0x10,
-    Failed = 0x11,
-}
 
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
 
 // Disable name mangling on this function
 // This function will be called on start as an entry point
