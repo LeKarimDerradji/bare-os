33e72246f74e521c107cfa49cdd5ba27e45de9de
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-09 23:16:08 +0100

    interupt

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,8 +1,15 @@
 #![feature(abi_x86_interrupt)]
 
-use x86_64::structures::{self, idt::InterruptDescriptorTable}:idt::InteruptDescriptorTable;
-
+use x86_64::structures::{self, idt::InterruptDescriptorTable};
+use crate::println;
 
 pub fn init_idt() {
     let mut idt = InterruptDescriptorTable::new();
+    idt.breakpoint.set_handler_fn(breakpoint_handler);
+}
+
+extern "x86-interrupt" fn breakpoint_handler(
+    stack_frame: InterruptStackFrame)
+{
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
 }
