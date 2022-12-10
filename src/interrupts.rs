4238078432bab86aac6334d4302e23ce48fce205
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-10 23:25:17 +0100

    init_idt_2

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,6 +6,7 @@ use crate::println;
 pub fn init_idt() {
     let mut idt = InterruptDescriptorTable::new();
     idt.breakpoint.set_handler_fn(breakpoint_handler);
+    idt.load();
 }
 
 extern "x86-interrupt" fn breakpoint_handler(
