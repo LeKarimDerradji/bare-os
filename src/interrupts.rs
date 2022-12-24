afca03509074401fbe00850a0b3b95bfbf886395
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-12-24 21:24:41 +0100

    double_fault_handler

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,4 +1,4 @@
-use crate::println;
+use crate::{println};
 use lazy_static::lazy_static;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
 
@@ -6,6 +6,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault.set_handler_fn(double_fault_handler);
         idt
     };
 }
@@ -14,8 +15,15 @@ pub fn init_idt() {
     IDT.load();
 }
 
-extern r#"x86-interrupt"# fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
     println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
 }
 
 #[test_case]
